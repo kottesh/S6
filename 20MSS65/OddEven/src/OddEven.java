58163ecb514cdc87b2c91fbac9f015a578a742d2
Shree Kottes <kottezh@gmail.com>
2025-04-20 18:58:26 +0530

    add basic junit programs

---
+++ 20MSS65/OddEven/src/OddEven.java
@@ -0,0 +1,9 @@
+public class OddEven {
+    public boolean findOddOrEven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
